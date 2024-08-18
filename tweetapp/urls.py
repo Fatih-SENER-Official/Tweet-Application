cab61f9c7a3d81a29013afe3698ce276f580c4cd
Fatih SENER <fatihsener840@gmail.com>
2024-08-18 18:42:28 +0300

    Initial Commit

---
+++ tweetapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'tweetapp'
+
+urlpatterns = [
+    path('',views.listtweet,name='listtweet'),
+    path('addtweet/',views.addtweet,name='addtweet'),
+    path('addtweetbyform',views.addtweetbyform,name='addtweetbyform'),
+    path('addtweetbymodelform',views.addtweetbymodelform,name='addtweetbymodelform'),
+]
---
+++ tweetapp/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from . import models
+from django.urls import reverse
+from tweetapp.forms import AddTweetForm, AddTweetModelForm
+
+# Create your views here.
+
+def listtweet(request):
+    all_tweets = models.Tweet.objects.all()
+    tweet_dict = {"tweets":all_tweets}
+    return render(request,'tweetapp/listtweet.html', context=tweet_dict)
+
+def addtweet(request):
+    if request.POST:
+        nickname = request.POST["nickname"]
+        message = request.POST["message"]
+        models.Tweet.objects.create(nickname=nickname, message=message)
+        return redirect(reverse('tweetapp:listtweet'))
+    else:
+        return render(request,'tweetapp/addtweet.html')
+    
+def addtweetbyform(request):
+    if request.method == "POST":
+        form = AddTweetForm(request.POST)
+        if form.is_valid():
+             nickname = form.cleaned_data["nickname_input"]
+             message = form.cleaned_data["message_input"]
+             models.Tweet.objects.create(nickname=nickname, message=message)
+             return redirect(reverse('tweetapp:listtweet'))
+        else:
+            print("error in form!")
+            return render(request,'tweetapp/addtweetbyform.html', context={"form":form})
+    else:
+        form = AddTweetForm()
+        return render(request,'tweetapp/addtweetbyform.html', context={"form":form})
+
+def addtweetbymodelform(request):
+    if request.method == "POST":
+        form = AddTweetModelForm(request.POST)
+        if form.is_valid():
+             nickname = request.POST["nickname"]
+             message = request.POST["message"]
+             models.Tweet.objects.create(nickname=nickname, message=message)
+             
+             return redirect(reverse('tweetapp:listtweet'))
+        else:
+            print("error in form!")
+            return render(request,'tweetapp/addtweetbymodelform.html', context={"form":form})
+    else:
+        form = AddTweetModelForm()
+        return render(request,'tweetapp/addtweetbymodelform.html', context={"form":form})
