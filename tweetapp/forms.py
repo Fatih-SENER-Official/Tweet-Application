0a8434bf257e9497a606aa2a6a811c5f612bac8e
Fatih SENER <fatihsener840@gmail.com>
2024-08-19 23:25:20 +0300

    Some updates have been done to the project

--- tweetapp/forms.py
+++ tweetapp/forms.py
@@ -3,11 +3,11 @@ from django.forms import ModelForm
 from tweetapp.models import Tweet
 
 class AddTweetForm(forms.Form):
-    nickname_input = forms.CharField(label="Nickname",max_length=50)
+    nickname_input = forms.CharField(label="Username",max_length=50)
     message_input = forms.CharField(label="Message",max_length=100,widget=forms.Textarea(attrs={"class":"tweetmessage"}))
 
 
 class AddTweetModelForm(ModelForm):
     class Meta:
         model = Tweet 
-        fields = ["nickname","message"]
+        fields = ["username","message"]
